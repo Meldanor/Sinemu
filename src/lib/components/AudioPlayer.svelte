05514c10e778382765377c1be54cd95e23c065d5
Kilian Gärtner <Bhaals@gmx.de>
2024-02-07 19:32:06 +0100

    add (audioplayer): basic audio player with howler

--- src/lib/components/AudioPlayer.svelte
+++ src/lib/components/AudioPlayer.svelte
@@ -1,17 +1,78 @@
 <script lang="ts">
+  import { Howl } from 'howler';
+  import AudioPlayerProgress from './AudioPlayerProgress.svelte';
+  import { onMount } from 'svelte';
+
   export let audioUrl: string;
 
-  let audioPlayer: HTMLAudioElement;
+  let audio: Howl;
 
-  $: audioUrl && audioPlayer && stopAudio();
+  $: audioUrl && switchTrack(audioUrl);
 
-  function stopAudio() {
-    audioPlayer.pause();
-    audioPlayer.load();
-    audioPlayer.play();
+  let playing = false;
+  let trackLength: number;
+  let currentPos: number;
+
+  function switchTrack(url: string) {
+    playing = false;
+    if (audio) {
+      audio.stop();
+    }
+
+    audio = new Howl({
+      src: url,
+      onload: () => {
+        trackLength = audio.duration();
+        currentPos = 0;
+        playing = true;
+        audio.play();
+      }
+    });
   }
+
+  function pauseOrResume() {
+    if (playing) {
+      pause();
+    } else {
+      resume();
+    }
+    playing = !playing;
+  }
+
+  function pause() {
+    audio.pause();
+  }
+
+  function resume() {
+    audio.play();
+  }
+
+  function updateCurrentPosition() {
+    requestAnimationFrame(() => {
+      if (audio) {
+        currentPos = audio.seek();
+      }
+      updateCurrentPosition();
+    });
+  }
+
+  onMount(() => {
+    updateCurrentPosition();
+  });
 </script>
 
-<audio bind:this={audioPlayer} controls preload="metadata">
-  <source src={audioUrl} />
-</audio>
+<div class="flex flex-col gap-8">
+  <div class="flex gap-3">
+    <button disabled={!audio} on:click={pauseOrResume}>
+      {#if playing}
+        ⏸
+      {:else}
+        ▶
+      {/if}
+    </button>
+    <!-- <button disabled={!audio} on:click={pauseOrResume}> ▶ ⏸</button> -->
+  </div>
+  {#if audio}
+    <AudioPlayerProgress {trackLength} {currentPos}></AudioPlayerProgress>
+  {/if}
+</div>
---
+++ src/lib/components/AudioPlayerProgress.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let trackLength: number;
+  export let currentPos: number;
+
+  $: trackLength && currentPos && updateWidth();
+
+  let progressBarWidth: number;
+
+  function updateWidth() {
+    progressBarWidth = (currentPos / trackLength) * 100.0;
+  }
+</script>
+
+<div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+  <div class="bg-blue-600 h-2.5 rounded-full" style={`width: ${progressBarWidth}%`}></div>
+</div>
